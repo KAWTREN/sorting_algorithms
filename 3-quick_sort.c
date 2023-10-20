e0717bbe6ca95ab169972328d95d321adc502e1d
kawtar <kawtarennabirha@gmail.com>
2023-10-20 09:36:47 -0700

    p

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,30 +1,63 @@
 #include "sort.h"
 /**
- * selection_sort - Sorts an array of integers using the Selection sort algo
+ * quick_sort - Sorts an array of integers using the Quick sort algorithm
  * @array: The array to be sorted
  * @size: The size of the array
  */
-void selection_sort(int *array, size_t size)
+void quick_sort(int *array, size_t size)
 {
-	size_t i, j, min_index;
-	int temp;
-
 	if (array == NULL || size < 2)
 		return;
-	for (i = 0; i < size - 1; i++)
+	_quick_sort(array, 0, size - 1, size);
+}
+/**
+ * quick_sort - Recursive function to perform the Quick sort
+ * @array: The array to be sorted
+ * @low: The low index of the partition
+ * @high: The high index of the partition
+ * @size: The size of the array
+ */
+void _quick_sort(int *array, int low, int high, size_t size)
+{
+	if (low < high)
 	{
-		min_index = i;
-		for (j = i + 1; j < size; j++)
+		int pivot_index = lomuto_partition(array, low, high, size);
+
+		_quick_sort(array, low, pivot_index - 1, size);
+		_quick_sort(array, pivot_index + 1, high, size);
+	}
+}
+/**
+ * lomuto_partition - Performs the Lomuto partition
+ * @array: The array to be partitioned
+ * @low: The low index of the partition
+ * @high: The high index of the partition
+ * @size: The size of the array
+ *
+ * Return: The index of the pivot element
+ */
+int lomuto_partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low - 1;
+	int j, temp;
+
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] < pivot)
 		{
-			if (array[j] < array[min_index])
-				min_index = j;
-		}
-		if (min_index != i)
-		{
+			i++;
 			temp = array[i];
-			array[i] = array[min_index];
-			array[min_index] = temp;
-			print_array(array, size);
+			array[i] = array[j];
+			array[j] = temp;
+			if (i != j)
+				print_array(array, size);
 		}
 	}
+	temp = array[i + 1];
+	array[i + 1] = array[high];
+	array[high] = temp;
+	if (i + 1 != high)
+		print_array(array, size);
+	return (i + 1);
 }

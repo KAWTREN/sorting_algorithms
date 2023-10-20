8d5632883787dfef4afb9ee15450124b061652b9
kawtar <kawtarennabirha@gmail.com>
2023-10-20 05:28:05 -0700

    p

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,11 +6,12 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current = (*list)->next;
+	listint_t *current;
 	listint_t *prev;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
+	current = (*list)->next;
 	while (current != NULL)
 	{
 		prev = current->prev;
